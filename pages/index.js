509548f7ee388832fdcdb9780fe54754288bdda2
Marcondes <mvm.sp11@gmail.com>
2021-05-08 17:17:19 -0300

    navigation

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,13 @@
-import { useState } from "react";
+import Link from 'next/link';
 
-function Visu(){
-
-    return( 
-    <div>
+function Home() {
+    return (<div>
         <h1>Home</h1>
-        <Contador />
-        <h2>Teste</h2>
-    </div>
-    )
+
+        <Link href="/sobre">
+            <a>Acessar página Sobre</a>
+        </Link>
+    </div>)
 }
 
-function Home(){
-
-    return( 
-    <div>
-        <h1>Marina sua linda, cuida do Koda pro Deco poder ir lá pra cima e dormir</h1>
-    </div>
-    )
-}
-
-
-function Contador(){
-
-    const [contador,setContador] = useState(1);
-
-    function adicione(){
-
-        setContador(contador +1 );
-    }
-
-    return (
-
-        <div>
-            <div>{ contador }</div>
-            <button onClick={ adicione }>Adicionar</button>
-        </div>
-    );
-
-}
-
-export default Home;
+export default Home
