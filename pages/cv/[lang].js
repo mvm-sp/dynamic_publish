cb85ee6e7f0df44312ad61e4f0bf166f57bdbb99
Marcondes <mvm.sp11@gmail.com>
2021-05-27 21:20:58 -0300

    Try URL

--- pages/cv/[lang].js
+++ pages/cv/[lang].js
@@ -163,7 +163,8 @@ export async function getStaticPaths() {
 export async function getStaticProps(context) {
 
     let idLang = (context.params.lang==='en'? '1':'1');
-    const URL_Back = `https://mvm-cv-back.herokuapp.com/api/v1/resume/1/${idLang}`
+    //const URL_Back = `https://mvm-cv-back.herokuapp.com/api/v1/resume/1/${idLang}`
+    const URL_Back = `${process.env.URL_CV_BASE}resume/1/${idLang}`
     //console.log(context.params.lang, idLang,(context.params.lang.toString()=='en') )
    // idLang = '1'
     let res = await fetch(URL_Back)
